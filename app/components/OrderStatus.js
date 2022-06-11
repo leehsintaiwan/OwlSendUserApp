a03d1a9676685793eed61f074fce0523b1b6622b
Vincent <vl620@ic.ac.uk>
2022-06-11 20:14:46 +0100

    Center minutes in minutes container Format time to allow for leading zero Lett minutes countdown automatically updating when system clock changes Cap minutes at minimum zero minutes not negative

--- app/components/OrderStatus.js
+++ app/components/OrderStatus.js
@@ -7,16 +7,33 @@ import { Text } from "react-native-elements";
 import { db } from "../core/Config";
 import Colors from "../core/Colors";
 
+const NANOSECONDS_IN_MINUTE = 60000;
+
 const PickupScreen = () => {
-  const [orderStatus, setOrderStatus] = useState(null);
   const userPhone = "0123456789";
   const orderDoc = doc(db, "UserOrders", userPhone);
+  const [orderStatus, setOrderStatus] = useState(null);
+  const [minutesLeft, setMinutesLeft] = useState(0);
 
   useEffect(() => {
     return onSnapshot(orderDoc, (doc) => {
       setOrderStatus(doc.data());
     });
   }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMinutesLeft(
+        Math.max(
+          0,
+          Math.ceil(
+            (orderStatus?.time.toDate() - Date.now()) / NANOSECONDS_IN_MINUTE
+          )
+        )
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [orderStatus]);
 
   return (
     <View style={styles.container}>
@@ -29,22 +46,27 @@ const PickupScreen = () => {
         <Text style={[styles.allText, styles.driverText]}>
           Your parcel will be picked up by:
         </Text>
-        <Text h2 style={[styles.allText, styles.driverText, styles.driverName]}>
-          {orderStatus?.driver.name}
-        </Text>
+        <View style={styles.driverNameContainer}>
+          <Text h2 style={[styles.allText, styles.driverName]}>
+            {orderStatus?.driver.name}
+          </Text>
+        </View>
       </View>
       <View style={styles.timeContainer}>
         <View style={styles.timeRow}>
           <Text style={[styles.allText]}>Your driver will be arriving at:</Text>
           <Text h2 style={[styles.allText, styles.time]}>
-            {orderStatus?.time.toDate().getHours()}:
-            {orderStatus?.time.toDate().getMinutes()}
+            {orderStatus?.time
+              .toDate()
+              .toLocaleTimeString("en-GB")
+              .substring(0, 5)}
           </Text>
         </View>
-        <Text h2 style={[styles.allText, styles.minutesLeft]}>
-          In {Math.floor((orderStatus?.time.toDate() - Date.now()) / 60000)}{" "}
-          mins
-        </Text>
+        <View style={styles.minutesContainer}>
+          <Text h2 style={[styles.allText]}>
+            In {minutesLeft} mins
+          </Text>
+        </View>
       </View>
     </View>
   );
@@ -53,11 +75,47 @@ const PickupScreen = () => {
 export default PickupScreen;
 
 const styles = StyleSheet.create({
+  allText: {
+    fontWeight: "700",
+    fontSize: 18,
+  },
+
   container: {
     position: "absolute",
     bottom: 0,
     height: "45%",
     width: "100%",
+  },
+
+  driverContainer: {
+    backgroundColor: Colors.primary,
+    height: "40%",
+  },
+
+  driverText: {
+    color: "white",
+    paddingLeft: 15,
+    paddingTop: 15,
+  },
+
+  driverNameContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+  },
+
+  driverName: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  minutesContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   statusContainer: {
@@ -66,40 +124,20 @@ const styles = StyleSheet.create({
     padding: 15,
   },
 
-  allText: {
-    fontWeight: "700",
-    fontSize: 18,
-  },
-
-  driverContainer: {
-    backgroundColor: Colors.primary,
-    height: "40%",
-    // padding: 15,
-  },
-  driverText: {
-    color: "white",
-    padding: 15,
-  },
-  driverName: {
-    textAlign: "center",
-  },
   timeContainer: {
     height: "40%",
-    paddingLeft: 15,
-    paddingTop: 5,
   },
+
   timeRow: {
     display: "flex",
     flexDirection: "row",
-    width: "100%",
     alignItems: "center",
+    paddingLeft: 15,
+    paddingTop: 5,
   },
+
   time: {
     flexGrow: 1,
     textAlign: "center",
-  },
-  minutesLeft: {
-    textAlign: "center",
-    padding: 15,
   },
 });
