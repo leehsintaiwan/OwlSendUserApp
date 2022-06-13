4d140aa458122c2c3c43e7014e668c54d26b3155
Vincent <vl620@ic.ac.uk>
2022-06-13 11:25:08 +0100

    Finished Looking for drivers page

--- app/components/FindingDrivers.js
+++ app/components/FindingDrivers.js
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
+import Colors from "../core/Colors";
 
 const FindingDrivers = () => {
   return (
     <View style={styles.container}>
-      <Text>FindingDrivers</Text>
+      <View style={styles.labelContainer}>
+        <Text h2 style={[styles.text]}>
+          Looking for Drivers
+        </Text>
+        <Image
+          source={require("../assets/waiting.png")}
+          style={styles.waiting}
+        />
+      </View>
+      <Image
+        source={require("../assets/finding-drivers.gif")}
+        style={styles.gif}
+      />
     </View>
   );
 };
@@ -16,7 +30,32 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     position: "absolute",
     bottom: 0,
-    height: "45%",
+    height: "35%",
     width: "100%",
+  },
+
+  gif: {
+    width: "100%",
+    height: "45%",
+  },
+
+  labelContainer: {
+    backgroundColor: Colors.primary,
+    height: "45%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+  },
+
+  text: {
+    color: "white",
+    fontWeight: "700",
+  },
+
+  waiting: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    right: 15,
   },
 });
